1c55a177d2c161fd74a0626c89673ebcfb4c4a56
MR.HRCXXII <betazona1984@gmail.com>
2022-12-26 23:39:09 +0900

    Update torjan.sh

--- torjan.sh
+++ torjan.sh
@@ -1,24 +1,24 @@
 clear
 figlet TORJAN |lolcat
 echo "\33[0;36m============================================"
-echo "=     \33[31;1mSCURITY TORJAN CYBER INDONESIAN      ="
+echo "=     \33[31;1mSCURITY TORJAN TEAM: CYBER-XXII      ="
 echo "=       \33[31;1m            TORJAN                 ="
-echo "=     \33[31;1m         Author : Roni YT            ="
+echo "=     \33[31;1m         Author : Mr.XXII            ="
 echo "\33[0;36m============================================"
 echo "\33[0;36m                @@@@@@@@@@@@@@@          @@"
 echo "\33[0;36m                @1234567891011@ ========@  @"
 echo "\33[0;36m                @@@@@@@@@@@@@@@          @@"
 echo
-echo "\33[33;1mKetik 8 Untuk Melanjutkan Torjan"
-echo "\33[33;1mKetik 5 Untuk Keluar"
+echo "\33[33;1mKetik 1 Untuk Melanjutkan Torjan"
+echo "\33[33;1mKetik 0 Untuk Keluar"
 read -p "Masukan Pilihan Anda :" roni
 
 case $roni in
-8)
+1)
 clear
 python2 torjancyber.py
 ;;
-5)
+0)
 echo "Keluar"
 ;;
 *)
